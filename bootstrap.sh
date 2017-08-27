653a1d86c00dcdc661977078f09f9b7d44cc8fd1
aleksey.stryukov <aleksey.stryukov@gmail.com>
2017-08-27 14:42:31 +0300

    N14

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
-
-sudo apt-get update
-sudo apt-get install -y python3-dev
-sudo apt-get install -y python3-pip
-sudo pip3 install --upgrade pip
-sudo pip3 install jupyter
-sudo apt-get install -y python3-pandas
-sudo pip install pandas-datareader
+apt-get update
+apt-get install -y python3-dev
+apt-get install -y python3-pip
+pip3 install --upgrade pip
+pip3 install jupyter
+pip3 install pandas==0.19.2
+pip3 install pandas-datareader
 
 tar xvzf /vagrant/ta-lib-0.4.0-src.tar.gz
 cd ta-lib/
 ./configure --prefix=/usr
 make
-sudo make install
-sudo pip3 install TA-Lib
+make install
+pip3 install TA-Lib
 
 # python3 -m pip install pygame --user
 # pip3 install tensorflow
