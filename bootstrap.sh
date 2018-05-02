6deffe63459055808aae00ba03c456ca321c855b
Aleksey Stryukov <aleksey.stryukov@gmail.com>
2018-05-02 10:49:38 +0300

    datareader and yahoo error fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,9 @@ apt-get update
 apt-get install -y python3-dev
 apt-get install -y python3-pip
 pip3 install --upgrade pip
-pip3 install  matplotlib
+pip3 install  matplotlib==2.1.2
 pip3 install jupyter
 pip3 install pandas==0.19.2
-pip3 install pandas-datareader
 
 tar xvzf /vagrant/ta-lib-0.4.0-src.tar.gz
 cd ta-lib/
